ad1d63d08474cc0028a2af6cafc6daf4cec79afc
Jesus Romero <jhromero@tekmexico.com>
2020-02-11 17:21:29 -0800

    Added retries to mongoose.

--- src/db/conversationsDB.ts
+++ src/db/conversationsDB.ts
@@ -1,9 +1,20 @@
+// // This one doesn't use import from structure because it makes the Promise property read-only
 import mongoose from 'mongoose';
+console.log('Connecting to mongoDB using mongoose.');
 
-mongoose.connect(process.env.MONGODB_URI as string, {
+const connectionWithRetry = (mongoose: any) => mongoose.connect(process.env.MONGODB_URI as string, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    connectTimeoutMS: 10000,
     useCreateIndex: true
-});
+}).catch(() => console.log('Connection error.'));
 
+connectionWithRetry(mongoose);
+
+mongoose.connection.on('error', () => {
+    console.log('Conection failed. Retrying in 5 seconds.')
+    setTimeout(connectionWithRetry, 5000, mongoose);
+})
+
+console.log('Exporting connection object.');
 export default mongoose;
